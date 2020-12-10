b61cffb021691e49fe0940ee5aec05c4a2895b5f
Alexander Abnosov <abnosov@gmail.com>
2020-12-10 21:45:46 +0300

    initial

---
+++ main.py
@@ -0,0 +1,65 @@
+import os
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+options = Options()
+options.headless = True
+browser = webdriver.Chrome(
+    executable_path="./webdriver/chromedriver",
+    options=options
+)
+
+# URL to translate file names.
+url = "https://translate.google.com/?ui=tob&sl=auto&tl=en&op=translate"
+# URL to translate text files.
+url_docs = "https://translate.google.com/?ui=tob&sl=auto&tl=en&op=docs"
+
+text_files = os.listdir("./data/компьютеры/")
+for text_file in text_files:
+    print(text_file)
+    browser.get(url)
+    text_data = open(f"./data/компьютеры/{text_file}", "r").read()
+
+    input_area = browser.find_element_by_xpath(
+        '//*[@id="yDmH0d"]/c-wiz/div/div[2]/c-wiz/div[2]/'
+        'c-wiz/div[1]/div[2]/div[2]/c-wiz[1]/span/span/div/textarea'
+    )
+    input_area.click()
+    time.sleep(1)
+    input_area.send_keys(text_file.replace(".txt", ""))
+    time.sleep(1)
+
+    file_name = browser.find_element_by_xpath(
+        '//*[@id="yDmH0d"]/c-wiz/div/div[2]/c-wiz/div[2]/'
+        'c-wiz/div[1]/div[2]/div[2]/c-wiz[2]/div[5]/'
+        'div/div[1]/span[1]/span/span'
+    ).text
+
+    browser.get(url_docs)
+    file_input = browser.find_element_by_xpath(
+        '//*[@id="i34"]'
+    )
+    file_input.send_keys(
+        f"/home/alexander/projects/misc/translate/data/компьютеры/{text_file}"
+    )
+    time.sleep(1)
+
+    translate_button = browser.find_element_by_xpath(
+        '//*[@id="yDmH0d"]/c-wiz/div/div[2]/c-wiz/div[3]/c-wiz/div[2]/c-wiz/'
+        'div/form/div[2]/div[2]/button/span'
+    )
+    translate_button.click()
+    time.sleep(5)
+    text = str(browser.find_element_by_tag_name(
+        "pre"
+    ).get_attribute("innerHTML").replace("&lt;", "<").replace("&gt;", ">"))
+
+    with open(f"./results/it/{file_name}.txt", "w+") as new_file:
+        new_file.write(text)
+
+    os.remove(f"./data/компьютеры/{text_file}")
+
+browser.quit()
